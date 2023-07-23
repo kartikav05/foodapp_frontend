3d9c36e8e256c433add3ef0caae27655733ad00d
Kartik Avadhani <kartikav2001@gmail.com>
2023-07-24 00:06:19 +0530

    changes to homepage

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Delete from '@material-ui/icons/Delete'
 import { useCart, useDispatchCart } from '../components/ContextReducer';
 export default function Cart() {
   let data = useCart();
@@ -58,7 +57,7 @@ export default function Cart() {
                 <td>{food.qty}</td>
                 <td>{food.size}</td>
                 <td>{food.price}</td>
-                <td ><button type="button" className="btn p-0"><Delete onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button> </td></tr>
+                <td ><button type="button" className="btn btn-danger p-0" onClick={() => { dispatch({ type: "REMOVE", index: index }) }}>Remove</button> </td></tr>
             ))}
           </tbody>
         </table>

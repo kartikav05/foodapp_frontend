cbb977a4f50be098d2d40ffb8e5b4d04fe135155
Kartik Avadhani <kartikav2001@gmail.com>
2023-07-26 20:26:33 +0530

    made changes

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,7 +8,7 @@ export default function Carousel() {
 
                 <div className="carousel-inner " id='carousel'>
                     <div class=" carousel-caption  " style={{ zIndex: "9" }}>
-                        <form className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
+                        <form className=" d-flex justify-content-center"> 
                             <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Type in..." aria-label="Search" />
                             <button className="btn text-white bg-success" type="submit">Search</button>
                         </form>
